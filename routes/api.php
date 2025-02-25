eb4ee1ef7e0613b4573ac30cc6292767c8454659
Abdelhalim LAKFIFI <abdelhalimlakfifi@gmail.com>
2025-02-25 15:30:21 +0000

    Add API routes for product creation and category retrieval

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +17,12 @@ use App\Http\Controllers\Api\ProductController;
 */
 
 Route::prefix('v1')->group(function () {
+    // Products
     Route::get('/products', [ProductController::class, 'getAll']);
+    Route::post('/products', [ProductController::class, 'create']);
+
+    // Categories
+    Route::get('/categories', [CategoryController::class, 'getAll']);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {

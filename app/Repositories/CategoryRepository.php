70af047d1d9345f0ec17ef306f786054a4228c64
Abdelhalim LAKFIFI <abdelhalimlakfifi@gmail.com>
2025-02-25 13:03:16 +0000

    Add CategoryRepository with CRUD methods and bind to service container

---
+++ app/Repositories/CategoryRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Category;
+
+class CategoryRepository
+{
+    // Create a Category
+    public function create(array $data)
+    {
+        return Category::create($data);
+    }
+
+    // Get all Categories
+    public function all()
+    {
+        return Category::all();
+    }
+
+    // Find a Category by ID
+    public function findById($id)
+    {
+        return Category::findOrFail($id);
+    }
+
+    // Update a Category
+    public function update($id, array $data)
+    {
+        $category = Category::findOrFail($id);
+        $category->update($data);
+        return $category;
+    }
+
+    // Delete a Category
+    public function delete($id)
+    {
+        $category = Category::findOrFail($id);
+        $category->delete();
+    }
+}
